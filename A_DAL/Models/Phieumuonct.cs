3fb8b7fde69f7f7fc8a789a59628e5e746bede04
THAODTPPH43287\dtpth <thaodtpph43287@fpt.edu.vn>
2023-11-29 22:55:12 +0700

    DFHSDA

---
+++ A_DAL/Models/Phieumuonct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class Phieumuonct
+    {
+        public Phieumuonct()
+        {
+            Phieutracts = new HashSet<Phieutract>();
+        }
+
+        public int Id { get; set; }
+        public int Idsachct { get; set; }
+        public int Idphieumuon { get; set; }
+        public string? Ghichu { get; set; }
+
+        public virtual Phieumuon IdphieumuonNavigation { get; set; } = null!;
+        public virtual Sachchitiet IdsachctNavigation { get; set; } = null!;
+        public virtual ICollection<Phieutract> Phieutracts { get; set; }
+    }
+}
---
+++ A_DAL/Models/Sachchitiet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_DAL.Models
+{
+    public partial class Sachchitiet
+    {
+        public Sachchitiet()
+        {
+            Phieumuoncts = new HashSet<Phieumuonct>();
+            SachctHangs = new HashSet<SachctHang>();
+        }
+
+        public int Id { get; set; }
+        public int Idsach { get; set; }
+        public int Idngonngu { get; set; }
+        public int Idnxb { get; set; }
+        public int? Lantaiban { get; set; }
+        public decimal? Giasach { get; set; }
+        public int? Dotuoidocsach { get; set; }
+        public int? Tinhtrang { get; set; }
+
+        public virtual Ngonngu IdngonnguNavigation { get; set; } = null!;
+        public virtual Nxb IdnxbNavigation { get; set; } = null!;
+        public virtual Sach IdsachNavigation { get; set; } = null!;
+        public virtual ICollection<Phieumuonct> Phieumuoncts { get; set; }
+        public virtual ICollection<SachctHang> SachctHangs { get; set; }
+    }
+}
