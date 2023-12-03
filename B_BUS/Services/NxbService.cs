af884e9924365864296e5229dd84fdceaa90d35f
THAODTPPH43287\dtpth <thaodtpph43287@fpt.edu.vn>
2023-12-03 13:34:57 +0700

    OK

--- B_BUS/Services/NxbService.cs
+++ B_BUS/Services/NxbService.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using A_DAL.Models1;
+using A_DAL.Repository;
 
 namespace B_BUS.Services
 {
     public class NxbService
+
     {
+        NxbRepos _repos = new NxbRepos();
+        public NxbService()
+        {
+
+        }
+
+        public NxbService(NxbRepos repos)
+        {
+            _repos = repos;
+        }
+
+        public IEnumerable<Nxb> GetAll()
+        {
+            return _repos.GetAllHang().ToList();
+        }
+        public string add(Nxb nxb)
+        {
+            if (_repos.Add(nxb) == 2)
+            {
+                return "Thêm thành công";
+            }
+            else if (_repos.Add(nxb) == 1)
+            {
+                return "Tên tác giả không được để trống";
+            }
+            else
+            {
+                return "Thêm thất bại";
+            }
+        }
+        public bool Delete(int id)
+        {
+            return _repos.Delete(id);
+        }
+
+
+        public List<Nxb> GetSearch(string searchText)
+        {
+            return _repos.GetSearch(searchText);
+        }
+        public int Update(int id, Nxb nxb)
+        {
+            return _repos.Update(id, nxb);
+        }
     }
 }
