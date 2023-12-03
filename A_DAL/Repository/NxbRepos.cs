d313732e755167efc97f0e800c17c065834a3dc1
THAODTPPH43287\dtpth <thaodtpph43287@fpt.edu.vn>
2023-12-03 13:33:55 +0700

    NXB

--- A_DAL/Repository/NxbRepos.cs
+++ A_DAL/Repository/NxbRepos.cs
@@ -3,10 +3,112 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using A_DAL.IRepository;
+using A_DAL.Models1;
 
 namespace A_DAL.Repository
 {
-    public class NxbRepos
+    public class NxbRepos: INxbRepos
     {
+        DUAN1Context _dbContext = new DUAN1Context();
+        public NxbRepos()
+        {
+
+        }
+
+        public NxbRepos(DUAN1Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public int Add(Nxb nxb)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(nxb.Tennxb))
+                {
+                    return 1;
+                }
+                else
+                {
+                    _dbContext.Nxbs.Add(nxb);
+                    _dbContext.SaveChanges();
+                    return 2;
+                }
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var exist = _dbContext.Nxbs.Find(id);
+
+
+                _dbContext.Nxbs.Remove(exist);
+                _dbContext.SaveChanges();
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        public List<Nxb> GetSearch(string searchText)
+        {
+            if (string.IsNullOrWhiteSpace(searchText))
+            {
+                return _dbContext.Nxbs.ToList();
+            }
+            return _dbContext.Nxbs.Where(c => c.Tennxb.Contains(searchText)).ToList();
+        }
+
+
+
+        public int Update(int id, Nxb nxb)
+        {
+            try
+            {
+                var exist = _dbContext.Nxbs.Find(id);
+                if (exist == null)
+                {
+                    return 1;
+                }
+                else if (string.IsNullOrWhiteSpace(nxb.Tennxb))
+                {
+                    return 2;
+                }
+                else
+                {
+                    exist.Tennxb = nxb.Tennxb;
+
+                    exist.Ghichu = nxb.Ghichu;
+                    exist.Diachi = nxb.Diachi;
+                    exist.Sdt = nxb.Sdt;
+                    _dbContext.Nxbs.Update(exist);
+                    _dbContext.SaveChanges();
+                    return 3;
+                }
+
+            }
+            catch
+            {
+                return 1;
+            }
+        }
+
+        public IEnumerable<Nxb> GetAll()
+        {
+            return _dbContext.Nxbs.ToList();
+        }
     }
 }
